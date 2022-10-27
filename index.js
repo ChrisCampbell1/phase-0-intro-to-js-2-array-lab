3e6e17f767d3e37a6e7f246be83c81ffe3dd6fe7
Chris Campbell <christopherecampbell@gmail.com>
2022-10-27 15:27:58 -0600

    commit tree 51cdfc161bfac88b61c44912b317e7ac71e0b9a1

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo" , "Otis" , "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat (name){
+    const newCats = [...cats]
+    newCats.push(name)
+    return newCats
+}
+
+function prependCat(name){
+    const  newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+
+function removeLastCat(name){
+    const newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat(name){
+    const newCats = [...cats]
+    newCats.splice(0,1)
+    return newCats
+}
+
+//console.log(newCats)
